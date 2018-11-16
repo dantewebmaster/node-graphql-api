4d3574d608e2de549ab16cdeeed64fe56af6087e
Dante Webmaster <dantewebmaster@outlook.com>
2018-11-16 03:41:52 -0200

    Post schema: types, queries and mutations

--- dist/graphql/schema.js
+++ dist/graphql/schema.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const graphql_tools_1 = require("graphql-tools");
 const query_1 = require("./query");
 const mutation_1 = require("./mutation");
+const post_schema_1 = require("./resources/post/post.schema");
 const user_schema_1 = require("./resources/user/user.schema");
 const SchemaDefinition = `
   type Schema {
@@ -15,6 +16,7 @@ exports.default = graphql_tools_1.makeExecutableSchema({
         SchemaDefinition,
         query_1.Query,
         mutation_1.Mutation,
+        post_schema_1.postTypes,
         user_schema_1.userTypes,
     ]
 });
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,6 +1,7 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import { Query } from './query';
 import { Mutation } from './mutation';
+import { postTypes } from './resources/post/post.schema';
 import { userTypes } from './resources/user/user.schema';
 
 const SchemaDefinition = `
@@ -15,6 +16,7 @@ export default makeExecutableSchema({
     SchemaDefinition,
     Query,
     Mutation,
+    postTypes,
     userTypes,
   ]
 });
