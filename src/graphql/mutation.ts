b07de96900abe95b3475306d49535875ae4ed5f2
Dante Webmaster <dantewebmaster@outlook.com>
2018-11-20 03:38:49 -0200

    JWT implemented

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -1,9 +1,11 @@
+import { tokenMutations } from './resources/token/token.schema';
 import { commentMutations } from './resources/comment/comment.schema';
 import { postMutations } from './resources/post/post.schema';
 import { userMutations } from './resources/user/user.schema';
 
 const Mutation = `
   type Mutation {
+    ${tokenMutations}
     ${commentMutations}
     ${postMutations}
     ${userMutations}
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -2,6 +2,7 @@ import { makeExecutableSchema } from 'graphql-tools';
 import { merge } from 'lodash';
 import { Query } from './query';
 import { Mutation } from './mutation';
+import { tokenTypes } from './resources/token/token.schema';
 import { commentTypes } from './resources/comment/comment.schema';
 import { postTypes } from './resources/post/post.schema';
 import { userTypes } from './resources/user/user.schema';
@@ -9,8 +10,10 @@ import { userTypes } from './resources/user/user.schema';
 import { commentResolvers } from './resources/comment/comment.resolvers';
 import { postResolvers } from './resources/post/post.resolvers';
 import { userResolvers } from './resources/user/user.resolvers';
+import { tokenResolvers } from './resources/token/token.resolvers';
 
 const resolvers = merge(
+  tokenResolvers,
   commentResolvers,
   postResolvers,
   userResolvers,
@@ -25,6 +28,7 @@ const SchemaDefinition = `
 
 export default makeExecutableSchema({
   typeDefs: [
+    tokenTypes,
     SchemaDefinition,
     Query,
     Mutation,
