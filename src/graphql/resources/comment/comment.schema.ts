8a97b25d783079d92e3e5fd13b612605baf50e4d
Dante Webmaster <dantewebmaster@outlook.com>
2018-11-20 00:36:54 -0200

    Queries and mutations revised and fixed one bug

--- src/graphql/resources/comment/comment.schema.ts
+++ src/graphql/resources/comment/comment.schema.ts
@@ -16,12 +16,12 @@ const commentTypes = `
 `;
 
 const commentQueries = `
-  commentsByPost(post: ID!, first: Int, offset: Int): [Comment!]!
+  commentsByPost(postId: ID!, first: Int, offset: Int): [Comment!]!
 `;
 
 const commentMutations = `
   createComment(input: CommentInput!): Comment
-  updateComment(input: CommentInput!): Comment
+  updateComment(id: ID!, input: CommentInput!): Comment
   deleteComment(id: ID!): Boolean
 `;
 
